dd86debaa44b550891025a454cfa6d10dcef15ff
ChristianTC <christiantambo15@gmail.com>
2020-10-13 20:21:20 -0400

    First plugin, play and mute

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -2,14 +2,29 @@
 
 //funcion mediaPLayer
 function MediaPlayer(config) {
-    this.media = config.el
+    this.media = config.el;
+    this.plugins = config.plugins || [];
+
+    this._initPlugins();
 };
+
+MediaPlayer.prototype._initPlugins = function() {
+    this.plugins.forEach(plugin => {
+        plugin.run(this);
+    });
+}
 
 MediaPlayer.prototype.pause = function() {
     this.media.pause()
 }
 MediaPlayer.prototype.play = function() {
     this.media.play()
+}
+MediaPlayer.prototype.mute = function() {
+    this.media.muted = true;
+}
+MediaPlayer.prototype.unmute = function() {
+    this.media.muted = false;
 }
 
 MediaPlayer.prototype.tooglePlay = function() {
@@ -23,4 +38,14 @@ MediaPlayer.prototype.tooglePlay = function() {
         : this.pause()
 }
 
+/**
+ * Mute and Unmute toogle
+ */
+MediaPlayer.prototype.toogleMute = function() {
+    console.log(this.media.muted);
+    
+    (this.media.muted === false)
+        ? this.mute()
+        : this.unmute()
+}
 export default MediaPlayer;
--- assets/index.js
+++ assets/index.js
@@ -1,14 +1,21 @@
 import MediaPlayer from './MediaPlayer.js'
+import AutoPlay from './plugins/AutoPlay.js'
 
 //Aqui haremos las modificaciones del video
 //a query selector hay que pasar un selector, un selector puede ser un video
 const video = document.querySelector("video")
 
 //ejecutamos el MediaPlayer
-const player = new MediaPlayer({ el: video })
+const player = new MediaPlayer({ el: video, plugins: [
+    new AutoPlay()
+] })
 
 // pasamos el selector button
-const button = document.querySelector("button")
+const button = document.querySelector("button.play")
+const buttonMute = document.querySelector("button.mute")
 
 // ejecutar el video con el boton 
 button.onclick = () => player.tooglePlay();
+
+// Mute and unmute 
+buttonMute.onclick = () => player.toogleMute();
